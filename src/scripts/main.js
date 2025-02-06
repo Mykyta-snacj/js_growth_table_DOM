e32d6da073cd5a7d917996b00570bd73f5aa2265
Mykyta Kalniev <nikitamal4ik@gmail.com>
2025-02-06 22:20:14 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,55 @@
 'use strict';
 
-// write code here
+const container = document.querySelector('.container');
+const field = document.querySelector('.field');
+const tbody = field.querySelector('tbody');
+
+// check actual size
+let actualRowsLength = field.querySelector('tbody').children.length;
+let actualColumnsLength = field.querySelector('tr').children.length;
+
+container.addEventListener('click', function (e) {
+  const row = field.querySelector('tr');
+
+  if (e.target.closest('.append-row')) {
+    const newRow = document.createElement('tr');
+
+    for (let i = 0; i < actualColumnsLength; i++) {
+      newRow.append(row.children[i].cloneNode());
+    }
+
+    if (actualRowsLength < 10) {
+      tbody.append(newRow);
+      actualRowsLength += 1;
+    }
+  }
+
+  if (e.target.closest('.remove-row')) {
+    if (actualRowsLength > 2) {
+      row.remove();
+      actualRowsLength -= 1;
+    }
+  }
+
+  if (e.target.closest('.append-column')) {
+    const newColumn = document.createElement('td');
+
+    if (actualColumnsLength < 10) {
+      actualColumnsLength += 1;
+
+      for (let i = 0; i < actualRowsLength; i++) {
+        tbody.children[i].append(newColumn.cloneNode());
+      }
+    }
+  }
+
+  if (e.target.closest('.remove-column')) {
+    if (actualColumnsLength > 2) {
+      actualColumnsLength -= 1;
+
+      for (let i = 0; i < actualRowsLength; i++) {
+        tbody.children[i].querySelector('td').remove();
+      }
+    }
+  }
+});
